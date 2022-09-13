e3703dc2926569c57226ebb4ffa19b63b1687f47
MrProha <mrprohagames@gmail.com>
2022-09-13 15:08:59 +0300

    proccess

--- code.py
+++ code.py
@@ -2,11 +2,12 @@ import re
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
-with open("phonebook_raw.csv") as f:
+with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-pprint(contacts_list)
-
+for i in contacts_list:
+    print(i)
+    
 class PhoneBook:
   def __init__(self, lastname, firstname, surname=None, organization=None, position=None, phone=None, email=None):
       self.lastname = lastname
