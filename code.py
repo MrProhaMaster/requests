67e8fe70893541c3cd2749e8bc20c60f0669206d
MrProha <mrprohagames@gmail.com>
2022-09-16 13:58:37 +0300

    homework done

--- code.py
+++ code.py
@@ -5,11 +5,9 @@ import csv
 with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-for i in contacts_list:
-    print(i)
-    
+
 class PhoneBook:
-  def __init__(self, lastname, firstname, surname=None, organization=None, position=None, phone=None, email=None):
+  def __init__(self, lastname, firstname, surname='', organization='', position='', phone='', email=''):
       self.lastname = lastname
       self.firstname = firstname
       self.surname = surname
@@ -19,13 +17,36 @@ class PhoneBook:
       self.email = email
 
   def __str__(self):
-    return f"{self.lastname} {self.firstname} {self.surname}: {self.organization}, {self.position}, {self.phone}, {self.email}"
+    return f"{self.lastname} {self.firstname} {self.surname},{self.organization},{self.position},{self.phone},{self.email}"
 
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
+for person in contacts_list:
+    if ' ' in person[0]:
+        a = contacts_list[contacts_list.index(person)][0].split(' ')
+        for i in a:
+            person[a.index(i)] = i
+    if ' ' in person[1]:
+        a = contacts_list[contacts_list.index(person)][1].split(' ')
+        for i in a:
+            person[a.index(i)+1] = i
+pattern = r"^(\+7|8)?\s*\(*(\d{1,3})\)*\s*\-*(\d{1,3})\s*\-*(\d{1,2})\s*\-*(\d{1,2})\s*\(*доб.\s*(\d*)\)*"
+for person in contacts_list[1:]:
+    if person in contacts_list:
+        contacts_list[contacts_list.index(person)][5] = re.sub(pattern, r"+7(\2)\3-\4-\5 доб.\6", person[5])
+        for check in contacts_list[1:]:
+            if person[0]+person[1] == check[0]+check[1] and contacts_list.index(person) < contacts_list.index(check):
+                for i in person:
+                    if i == '':
+                        if check[person.index(i)] == '':
+                            contacts_list[contacts_list.index(person)][person.index(i)] = ' '
+                        else:
+                            contacts_list[contacts_list.index(person)][person.index(i)] = contacts_list[contacts_list.index(check)][person.index(i)]
+                contacts_list.pop(contacts_list.index(check))
+for person in contacts_list[1:]:
+    for i in person:
+        if i == ' ':
+            contacts_list[contacts_list.index(person)][person.index(i)] = ''
 
 # TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
 with open("phonebook.csv", "w") as f:
   datawriter = csv.writer(f, delimiter=',')
   # Вместо contacts_list подставьте свой список
